d26d02108ca00544e6be4150dd4cfaec3d90608b
Duongtsnc1 <duongbui.hlnm@gmail.com>
2021-01-14 13:08:48 +0700

    init

---
+++ ShopProject/ShopProject/Areas/Administrator/AdministratorAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace ShopProject.Areas.Administrator
+{
+    public class AdministratorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Administrator";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                name: "Administrator_default",
+                url: "Administrator/{controller}/{action}/{id}",
+                defaults: new {action = "Login", id = UrlParameter.Optional }
+                
+            );
+        }
+        
+    }
+}
